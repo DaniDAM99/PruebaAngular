37f06d51e57178beebee5e7e83cb778d6da5a185
DaniDAM99 <muresandaniel3654@gmail.com>
2021-01-21 17:55:44 +0100

    subida dia 21

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
+import { NgModule, LOCALE_ID } from '@angular/core';
+import "@angular/common/locales/global/es"
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './componentes/home/home.component';
 import { AboutComponent } from './componentes/about/about.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { NavegacionComponent } from './componentes/navegacion/navegacion.component';
+import { FormsModule} from '@angular/forms';
+import { LoteriaComponent } from './componentes/loteria/loteria.component';
+import { GenerarloteriaComponent } from './componentes/generarloteria/generarloteria.component';
+import { MultiplicarComponent } from './componentes/multiplicar/multiplicar.component';
+import { CalculadoraComponent } from './componentes/calculadora/calculadora.component';
+import { SaludorutaComponent } from './componentes/saludoruta/saludoruta.component';
+import { TuberiasComponent } from './componentes/tuberias/tuberias.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AboutComponent,
     HomeComponent,
-    AboutComponent
+    NavegacionComponent,
+    LoteriaComponent,
+    GenerarloteriaComponent,
+    MultiplicarComponent,
+    CalculadoraComponent,
+    SaludorutaComponent,
+    TuberiasComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [{provide:LOCALE_ID, useValue:"es"}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
