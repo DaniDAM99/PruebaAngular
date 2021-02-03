753b07453a142d857e0e8c71b466fb7d6b97c80b
DaniDAM99 <muresandaniel3654@gmail.com>
2021-02-03 21:11:43 +0100

    commit dia 03

--- src/app/componentes/loteria/loteria.component.ts
+++ src/app/componentes/loteria/loteria.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class LoteriaComponent implements OnInit {
 
   constructor() { }
-
+  boton_pulsado: boolean = false
   ngOnInit(): void {
   }
 
