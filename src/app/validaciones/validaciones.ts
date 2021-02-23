713eb068fc1d720e6da9a162c67e836177c1f899
DaniDAM99 <muresandaniel3654@gmail.com>
2021-02-23 19:44:13 +0100

    validaciones form

--- src/app/validaciones/validaciones.ts
+++ src/app/validaciones/validaciones.ts
@@ -1,32 +1,25 @@
 import { AbstractControl, ValidatorFn } from "@angular/forms";
 
 export function telefonoValido(): ValidatorFn {
-    return (telefono: AbstractControl): {[key:string]:any} | null=>{
-        const numTelefono = telefono.value
-        if(numTelefono>600000000 && numTelefono<999999999) {
-            return null
-        }
-        return {telefono: "inválido"}
+    return (telefono: AbstractControl):{[key:string]:any} | null => {
+        const numTelefono = telefono.value;
+        if (numTelefono==null) return null;
+        if (numTelefono>999999999 || numTelefono<600000000){
+            return {telefono: "inválido"}
+        }else{return null}
     }
+    
 }
 
 export function dniValido(): ValidatorFn {
-    return (dni: AbstractControl): {[key:string]:any} | null=>{
-        const letras = "TRWAGMYFPDXBNJZSQVHLCKET"
-        const numero = dni.value.match(/\d+/g)
-        let letra = dni.value.match(/[a-z]/gi)
-        const letra_corracta = letras[numero%23]
-
-        if(letra == null) {
-            return {dni: "Falta la letra"}
-        }
-        else {
-            letra = letra.join().toUperCase()
-        }
-
-        if(letra_corracta == letra) {
-            return null
-        }
-        return {dni: "inválido"}
+    return (dni: AbstractControl):{[key:string]:any} | null => {
+        const letras = "TRWAGMYFPDXBNJZSQVHLCKET";
+        let numero = dni.value.match(/\d+/g);
+        let letra = dni.value.match (/[a-z]/gi);
+        const letra_eval = letras [numero%23];
+        if (letra == null)return {dni: "Falta la letra"};
+        else letra = letra.join().toUpperCase();
+        if (letra_eval != letra) return {dni: "inválido"}
+        else {return null}
     }
 }
